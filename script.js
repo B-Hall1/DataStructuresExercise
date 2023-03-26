38e072dac04206b7ef52e7c0fdf244f60340a5dc
Blair Hall <blair.hall.og@gmail.com>
2023-03-26 12:47:05 -0400

    Created Exercise

---
+++ script.js
@@ -0,0 +1,14 @@
+const jedi = [];
+jedi[0] = "Luke";
+jedi.push("Obi-Wan Kenobi");
+jedi.unshift("Yoda");
+console.log(jedi[1]);
+jedi.pop();
+const sithLords = [ "Darth Vader", "Darth Sidious", "Darth Maul"];
+const imperialOfficers = ["Grand Moff Tarkin", "Orson Krennic"];
+const starWarsVillains = sithLords.concat(imperialOfficers);
+console.log(starWarsVillains.slice(0,2));
+const droids = {astromech: "R2-D2", protocol: "C-3PO", assassin: "IG-88"};
+console.log(droids["astromech"]);
+console.log(droids.protocol);
+droids.assassin = "IG-11";
